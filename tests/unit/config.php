db330423435d0dcbe9aa74634738b834b86fbb4c
Nik Samokhvalov <nik@samokhvalov.info>
2019-03-07 19:08:50 +0300

    Added console command for tests send logs to Sentry

---
+++ tests/unit/config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'id' => 'sentry-tests',
+    'class' => 'yii\console\Application',
+    'basePath' => \Yii::getAlias('@tests'),
+    'runtimePath' => \Yii::getAlias('@tests/_output'),
+    'bootstrap' => [],
+];
